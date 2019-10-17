4f38bd1ee3b9ac7bcdc0a1cffb3aee7804ac669d
nancygoyal1206 <goyal.nancy1206@gmail.com>
2019-10-16 21:43:25 -0400

    server changes

--- src/httpServerAssign/HttpServer.java
+++ src/httpServerAssign/HttpServer.java
@@ -7,51 +7,95 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+//import org.json.*;
+//import org.json.simple.JSONObject;
 
 public class HttpServer {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		ServerSocket socketServer = new ServerSocket(8080);
-		System.out.println("Listening for connection on port 8080 ...."); 
+		System.out.println("Listening for connection on port 8080 ....");
 		while (true) {
 			BufferedOutputStream dataOut = null;
 			PrintWriter out = null;
-			Socket clientSocket =null;
+			Socket clientSocket = null;
 			try {
-		    clientSocket = socketServer.accept(); 
-			InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream()); 
-			BufferedReader reader = new BufferedReader(isr); 
-			out = new PrintWriter(clientSocket.getOutputStream());
-			dataOut = new BufferedOutputStream(clientSocket.getOutputStream());
-			String line = reader.readLine(); 
-			while (!line.isEmpty()) 
-			{ 
-			System.out.println(line); 
-			line = reader.readLine(); 
-			} 
-            String httpResponse = "hello" ;
-			out.println("HTTP/1.1 501 Not Implemented");
-			out.println("Content-length: " + httpResponse.length());
-			out.println(); // blank line
-			out.flush();
-           
-            dataOut.write(httpResponse.getBytes("UTF-8"));
-            dataOut.flush();
-            clientSocket.close();
-			}
-			catch (IOException ioe) {
+				clientSocket = socketServer.accept();
+				List<String> res =null;
+				InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
+				BufferedReader reader = new BufferedReader(isr);
+				out = new PrintWriter(clientSocket.getOutputStream());
+				dataOut = new BufferedOutputStream(clientSocket.getOutputStream());
+				String line = reader.readLine();
+				StringTokenizer parse = new StringTokenizer(line);
+				String method = parse.nextToken();
+				if (method.equalsIgnoreCase("GET")) {
+					 res=parseGetRequest(parse);
+				}
+				while (!line.isEmpty()) {
+					System.out.println(line);
+					line = reader.readLine();
+					
+				}
+
+				sendResponseToClient(out, dataOut,res);
+
+			} catch (IOException ioe) {
 				System.err.println("Server error : " + ioe);
 			} finally {
 				try {
 					dataOut.close();
-					clientSocket.close(); 
+					clientSocket.close();
+					
 				} catch (Exception e) {
 					System.err.println("Error closing stream : " + e.getMessage());
-				} 
+				}
 			}
 		}
-		
+
+	}
+
+	public static List<String> parseGetRequest(StringTokenizer parse)
+	{
+		String fileRequested = parse.nextToken();
+		List<String> result= null;
+		if(!fileRequested.trim().equalsIgnoreCase("/"))
+		{
+			System.out.println("\n\n fileRequested "+fileRequested);
 		}
+		else
+		{
+			try (Stream<Path> walk = Files.walk(Paths.get("./"))) {
+				
+				result = walk.filter(Files::isRegularFile)
+						.map(x -> x.getFileName().toString()).collect(Collectors.toList());
+
+				result.forEach(System.out::println);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	
+	public static void sendResponseToClient(PrintWriter out, BufferedOutputStream dataOut,List<String> httpResponse) throws IOException {
+		//JSONObject obj = new JSONObject();
+		out.println("HTTP/1.0 200 OK");
+		out.println("Content-length: " + httpResponse.toString().length());
+		out.println(); // blank line
+		out.flush();
+
+		dataOut.write(httpResponse.toString().getBytes("UTF-8"));
+		dataOut.flush();
+	}
 
 }
-
