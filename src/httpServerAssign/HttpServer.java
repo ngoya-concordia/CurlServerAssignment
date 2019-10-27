2ca6262d0bd1259dcf1d82037cb99401cdaf0bcc
Rohit Gupta <rohitguptarg24@gmail.com>
2019-10-27 06:00:34 -0400

    Chnages and Fixes

--- src/httpServerAssign/HttpServer.java
+++ src/httpServerAssign/HttpServer.java
@@ -14,21 +14,29 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-//import org.json.*;
-//import org.json.simple.JSONObject;
 
 public class HttpServer {
+	private ServerSocket socketServer;
+	private BufferedOutputStream dataOut = null;
+	private PrintWriter out = null;
+	private Socket clientSocket = null;
+	private static int portNo = 8080;
+	private static String serverFolder = "E://Concordia/Computer Networks/ServerData";
 
-	public static void main(String[] args) throws IOException {
-		ServerSocket socketServer = new ServerSocket(8080);
-		System.out.println("Listening for connection on port 8080 ....");
+	public HttpServer() throws IOException {
+		this(portNo, serverFolder);
+	}
+
+	public HttpServer(int portNo, String serverFolder) throws IOException {
+		HttpServer.portNo = portNo;
+		if (serverFolder.trim().length() > 0)
+			HttpServer.serverFolder = serverFolder;
+		socketServer = new ServerSocket(HttpServer.portNo);
+		System.out.println("Listening for connection on port : " + HttpServer.portNo);
 		while (true) {
-			BufferedOutputStream dataOut = null;
-			PrintWriter out = null;
-			Socket clientSocket = null;
 			try {
 				clientSocket = socketServer.accept();
-				List<String> res =null;
+				String res = "";
 				InputStreamReader isr = new InputStreamReader(clientSocket.getInputStream());
 				BufferedReader reader = new BufferedReader(isr);
 				out = new PrintWriter(clientSocket.getOutputStream());
@@ -36,16 +44,19 @@ public class HttpServer {
 				String line = reader.readLine();
 				StringTokenizer parse = new StringTokenizer(line);
 				String method = parse.nextToken();
+				System.out.println("Method Type  " + method);
 				if (method.equalsIgnoreCase("GET")) {
-					 res=parseGetRequest(parse);
+					res = parseGetRequest(parse);
+				} else {
+					res = parsePostRequest(parse);
 				}
 				while (!line.isEmpty()) {
 					System.out.println(line);
 					line = reader.readLine();
-					
+
 				}
 
-				sendResponseToClient(out, dataOut,res);
+				sendResponseToClient(out, dataOut, res);
 
 			} catch (IOException ioe) {
 				System.err.println("Server error : " + ioe);
@@ -53,7 +64,7 @@ public class HttpServer {
 				try {
 					dataOut.close();
 					clientSocket.close();
-					
+
 				} catch (Exception e) {
 					System.err.println("Error closing stream : " + e.getMessage());
 				}
@@ -62,33 +73,49 @@ public class HttpServer {
 
 	}
 
-	public static List<String> parseGetRequest(StringTokenizer parse)
-	{
+	private String parsePostRequest(StringTokenizer parse) {
+		// TODO Auto-generated method stub
 		String fileRequested = parse.nextToken();
-		List<String> result= null;
-		if(!fileRequested.trim().equalsIgnoreCase("/"))
-		{
-			System.out.println("\n\n fileRequested "+fileRequested);
-		}
-		else
-		{
-			try (Stream<Path> walk = Files.walk(Paths.get("./"))) {
-				
-				result = walk.filter(Files::isRegularFile)
-						.map(x -> x.getFileName().toString()).collect(Collectors.toList());
 
-				result.forEach(System.out::println);
+		return null;
+	}
 
-			} catch (IOException e) {
-				e.printStackTrace();
+	public String parseGetRequest(StringTokenizer parse) {
+		String fileRequested = parse.nextToken();
+		String result = "";
+//		try (Stream<Path> walk = Files.walk(Paths.get("./src/"))) {
+		System.out.println("Server Folder " + serverFolder);
+		try (Stream<Path> walk = Files.walk(Paths.get(serverFolder + "/"))) {
+
+			List<String> fileList = walk.filter(Files::isRegularFile).map(x -> x.getFileName().toString())
+					.collect(Collectors.toList());
+			if (!fileRequested.trim().equalsIgnoreCase("/")) {
+				fileRequested = fileRequested.trim().substring(1);
+				if (fileList.contains(fileRequested)) {
+//					Stream<String> stream = Files.lines(Paths.get("./src/httpServerAssign/" + fileRequested));
+					Stream<String> stream = Files.lines(Paths.get(serverFolder + "/" + fileRequested));
+
+					fileList = stream.collect(Collectors.toList());
+					result = fileList.toString();
+				} else {
+					System.out.println("Send Error Response 404");
+				}
+				System.out.println("\n\n fileRequested " + fileRequested);
+			} else {
+
+				result = fileList.toString();
+//				result.forEach(System.out::println);
+
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	
-	public static void sendResponseToClient(PrintWriter out, BufferedOutputStream dataOut,List<String> httpResponse) throws IOException {
-		//JSONObject obj = new JSONObject();
+
+	public void sendResponseToClient(PrintWriter out, BufferedOutputStream dataOut, String httpResponse)
+			throws IOException {
+		// JSONObject obj = new JSONObject();
 		out.println("HTTP/1.0 200 OK");
 		out.println("Content-length: " + httpResponse.toString().length());
 		out.println(); // blank line
---
+++ src/httpServerAssign/MainDriver.java
@@ -0,0 +1,73 @@
+package httpServerAssign;
+
+import java.util.Scanner;
+
+public class MainDriver {
+
+	private static boolean isDebug = false;
+	private static int portNo;
+	private static String serverFolder = "E://Concordia/Computer Networks/ServerData";
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		MainDriver mainDriver = new MainDriver();
+		int sel = mainDriver.displayFrontScreen();
+
+		while (true) {
+
+			if (sel == 1) {
+				scanner = new Scanner(System.in);
+				String input = scanner.nextLine();
+				String[] str = input.split(" -");
+
+				for (String s : str) {
+					System.out.println(s);
+					String[] test = s.split(" ");
+					String key = test[0].trim();
+					String value = "";
+					if (test.length > 1) {
+						for (int i = 1; test.length > i; i++)
+							value = value.concat(test[i].trim() + " ");
+					}
+
+					if (key.trim().equalsIgnoreCase("v"))
+						isDebug = true;
+					else if (key.trim().equalsIgnoreCase("p")) {
+						portNo = Integer.parseInt(value.trim());
+					} else if (key.trim().equalsIgnoreCase("d")) {
+						serverFolder = value.trim();
+					}
+				}
+
+				System.out.println("isDebug : " + isDebug);
+				System.out.println("portNo : " + portNo);
+				System.out.println("serverFolder : " + serverFolder);
+				HttpServer httpServer = null;
+				try {
+					if (portNo > 0)
+						httpServer = new HttpServer(portNo, serverFolder);
+					else
+						httpServer = new HttpServer();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if (sel == 2) {
+				System.out.print("===========EXIT the process================ ");
+				sel = mainDriver.displayFrontScreen();
+
+				break;
+			}
+
+		}
+
+	}
+
+	private int displayFrontScreen() {
+		System.out.println("*************************************************");
+		System.out.println("Welcome to the File Server Terminal");
+		System.out.println("*************************************************");
+		System.out.println("Press 1  to enter httpfs command");
+		System.out.println("Press 2 to Exit the process.");
+		return scanner.nextInt();
+	}
+}
